ed5804cf222286ca37e9e5fa8e807e72845d3e4e
Iuri Ribeiro <iuri.dias968@gmail.com>
2020-05-02 02:07:15 -0300

    Conversao

--- principal.js
+++ principal.js
@@ -1,7 +1,11 @@
 function calcular(binario) {
     var valorFinal = 0;
-    for (var i = 0; i < binario.length; i++) {
+    for (i = 0; i < binario.length; i++) {
+        if (Number(binario[i]) != 1 && Number(binario[i]) != 0) {
+            document.getElementById("convertido").innerHTML = "Valor inválido";
+            return -1;
+        }
         valorFinal += Number(binario[i]) * Math.pow(2, i);
     }
-    document.getElementById("convertido").innerHTML = "Valor convertido: " + valorFinal;
+    document.getElementById("convertido").innerHTML = "Valor em Decimal: " + valorFinal;
 }
